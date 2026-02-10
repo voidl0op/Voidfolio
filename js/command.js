835b434050b01025cbee8789ade536a62f360188
Salmane Sarghini <ss.sarghini@gmail.com>
2026-02-10 12:23:44 +0100

    reworked the UI and added the cd command

--- js/command.js
+++ js/command.js
@@ -1,11 +1,45 @@
 const main = document.querySelector("main");
 
+function createHelpLine(command, description) {
+  const line = document.createElement("p");
+  line.className = "preprecommand unselectable";
+  line.textContent = `${command} - ${description}`;
+  return line;
+}
+
+function help() {
+  const helpDiv = document.createElement("div");
+  helpDiv.appendChild(createHelpLine("ls", "browse the projects"));
+  helpDiv.appendChild(createHelpLine("cd", "socials"));
+  return helpDiv;
+}
+
+function createSocialLine(name, href, className) {
+  const line = document.createElement("a");
+  line.className = className + " unselectable";
+  line.textContent = name;
+  line.href = href;
+  line.target = "_blank";
+  line.rel = "noopener noreferrer";
+  return line;
+}
+
+function socials() {
+  const socialDiv = document.createElement("div");
+  socialDiv.className = "socials";
+  socialDiv.appendChild(createSocialLine("github", "https://github.com/voidl0op", "github-link"));
+  socialDiv.appendChild(document.createElement("br"));
+  socialDiv.appendChild(createSocialLine("linkedin", "https://www.linkedin.com/in/salmane-sarghini-147b673aa/", "linkedin-link"));
+
+  return socialDiv;
+}
+
 function createCommandLine() {
   const line = document.createElement("div");
 
   const user = document.createElement("p");
   user.className = "preprecommand unselectable";
-  user.textContent = "user@lazyfolio:";
+  user.textContent = "> user@voidfolio:";
 
   const prompt = document.createElement("p");
   prompt.className = "precommand unselectable";
@@ -28,22 +62,30 @@ function createCommandLine() {
       input.disabled = true;
       input.classList.add("unselectable");
 
-      const output = document.createElement("p");
-      output.className = "output";
+      const outputLine = document.createElement("div");
 
       switch (value) {
-        case "Hello":
-          output.textContent = "You typed Hello!";
+        case "help":
+          {
+            const helpContent = help();
+            outputLine.appendChild(helpContent);
+          }
           break;
-        case "Hi":
-          output.textContent = "You typed Hi!";
+        case "cd":
+          {
+            const socialsContent = socials();
+            outputLine.appendChild(socialsContent);
+          }
           break;
         default:
-          output.textContent = `Unknown command: ${value}`;
+          {
+            const output = document.createElement("p");
+            output.className = "output";
+            output.textContent = `Unknown command: ${value}`;
+            outputLine.appendChild(output);
+          }
       }
 
-      const outputLine = document.createElement("div");
-      outputLine.appendChild(output);
       main.appendChild(outputLine);
 
       createCommandLine();
@@ -52,4 +94,3 @@ function createCommandLine() {
 }
 
 createCommandLine();
-// i hate javascript
